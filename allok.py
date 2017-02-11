9539a38c56c1c7cc555ea1e6ef642375addd4ea0
Aayush Aggarwal <aayush16002@iiitd.ac.in>
2017-02-11 15:54:44 +0530

    Add files via upload

---
+++ allok.py
@@ -0,0 +1,40 @@
+import Tkinter as tk
+from Tkinter import *
+from PIL import ImageTk, Image
+import tkFont
+import os
+import time
+import sys
+
+class Page(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+    def show(self):
+        self.lift()
+
+class Page1(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text=" You seem to have no heart problems.\n That's the way to live!",font=tkFont.Font(family="Helvetica",size=12))
+       label.pack(side="top", fill="both", expand=True)
+
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        p1 = Page1(self)
+        buttonframe = tk.Frame(self)
+        container = tk.Frame(self)
+        buttonframe.pack(side="top", fill="x", expand=False)
+        container.pack(side="top", fill="both", expand=True)
+
+        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        if(int(sys.argv[1])<100):
+          p1.show()
+        
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    main = MainView(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.wm_geometry("400x400")
+    root.mainloop()

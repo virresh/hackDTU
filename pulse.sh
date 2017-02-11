af2ca15b4ee9c62531906ea743cf089917a1aa16
Viresh Gupta <viresh16118@iiitd.ac.in>
2017-02-11 14:55:35 +0530

    Condition Corrected

--- pulse.sh
+++ pulse.sh
@@ -2,7 +2,7 @@
 min=100
 x=`python get_pulse.py`
 y=`echo "$x" | sort -n | awk '{a[NR]=$0}END{if(NR%2==1)print a[int(NR/2)+1];else print(a[NR/2-1]+a[NR/2])/2}'`
-if [[ 1 -eq "$(echo "${y} < ${min}" | bc)" ]]
+if [[ 1 -eq "$(echo "${y} > ${min}" | bc)" ]]
 then
 	echo "Signs of 'Trachycardia' Possible"
 else

b4ec028bcc4d5361e26a590a590d798498062d73
Viresh Gupta <viresh16118@iiitd.ac.in>
2017-02-11 06:50:36 +0530

    Grid Layout Applied

--- tkinter.py
+++ tkinter.py
@@ -1,4 +1,4 @@
-from Tkinter import *
+# from Tkinter import *
 import Tkinter as tk
 import tkFont
 q = 0
@@ -8,12 +8,13 @@ correct = 0
 incorrect = 0
 question = ["Answer is 1","Answer is 2","Answer is 3","Answer is 4"]
 answer = [1,2,3,4]
-root = Tk()
+root = tk.Tk()
 fnt=tkFont.Font(family="Helvetica",size=12)
 name = tk.Label(root,text = "GUI Quiz",font=fnt)
-name.pack()
+name.grid(row=0,column=1)
 label = tk.Label(root,text = question[0],font=fnt)
-label.pack()
+label.grid(row=1,column=1)
+
 def out(par):
     global q,correct,incorrect,s,count
     count = count + 1
@@ -37,13 +38,13 @@ rad = tk.Radiobutton(root, text='First Option', variable=corr, value=1,font=fnt)
 rad2 = tk.Radiobutton(root, text='Second', variable=corr, value=2,font=fnt)
 rad3=tk.Radiobutton(root, text='Third', variable=corr, value=3,font=fnt)
 rad4=tk.Radiobutton(root, text='Fourth', variable=corr, value=4,font=fnt)
-rad.pack()
-rad2.pack()
-rad3.pack()
-rad4.pack()
-button = tk.Button(root,text = "Next",command = lambda:out(corr.get()),font=fnt)
-button.pack()
+rad.grid(row=2,column=0)
+rad2.grid(row=2,column=1)
+rad3.grid(row=2,column=2)
+rad4.grid(row=2,column=3)
+button = tk.Button(root,text = "Next",command = lambda:out((corr.get())%4),font=fnt)
+button.grid(row=3,column=2)
 button_two = tk.Button(root,text = "Restart",command = stop,font=fnt)
-button_two.pack()
-root.geometry("400x400")
+button_two.grid(row=3,column=1)
+# root.geometry("400x400")
 root.mainloop()

53ddbead511df9916275ac1eab997c5162608ebe
Viresh Gupta <viresh16118@iiitd.ac.in>
2017-02-11 14:53:06 +0530

    Floating point patch

--- pulse.sh
+++ pulse.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+min=100
 x=`python get_pulse.py`
 y=`echo "$x" | sort -n | awk '{a[NR]=$0}END{if(NR%2==1)print a[int(NR/2)+1];else print(a[NR/2-1]+a[NR/2])/2}'`
-if [[ "$y" -gt "100" ]]; then
+if [[ 1 -eq "$(echo "${y} < ${min}" | bc)" ]]
+then
 	echo "Signs of 'Trachycardia' Possible"
 else
 	echo "Healthy Life Owner Identified"

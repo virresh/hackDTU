a7bdc4d91e2d338bc4184a2a8d1959522fdbb708
Viresh Gupta <viresh16118@iiitd.ac.in>
2017-02-11 14:22:30 +0530

    Cleaned up repository

--- pulse.sh
+++ pulse.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 x=`python get_pulse.py`
-echo "$x" | sort -n|awk '{a[NR]=$0}END{if(NR%2==1)print a[int(NR/2)+1];else print(a[NR/2-1]+a[NR/2])/2}'
+y=`echo "$x" | sort -n | awk '{a[NR]=$0}END{if(NR%2==1)print a[int(NR/2)+1];else print(a[NR/2-1]+a[NR/2])/2}'`
+if [[ "$y" -gt "100" ]]; then
+	echo "Signs of 'Trachycardia' Possible"
+else
+	echo "Healthy Life Owner Identified"
+fi
